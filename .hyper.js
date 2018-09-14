89f6a1268c5e2dafc81987503a53f49955ee8cd1
Yuxiang Dai <YDai@indigo.ca>
2018-09-14 10:34:51 -0400

    Add dotfiles

---
+++ .hyper.js
@@ -0,0 +1,139 @@
+﻿module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 15,
+
+    // font family with optional fallbacks
+    fontFamily: "Consolas",
+
+    // default font weight: 'normal' or 'bold'
+    fontWeight: 'normal',
+
+    // font weight for bold characters: 'normal' or 'bold'
+    fontWeightBold: 'bold',
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#fff',
+
+    // terminal text color under BLOCK cursor
+    cursorAccentColor: '#000',
+
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+    cursorShape: 'BEAM',
+
+    // set to `true` (without backticks and without quotes) for blinking cursor
+    cursorBlink: true,
+
+    // color of the text
+    foregroundColor: '#f2f2f2',
+
+    // terminal background color
+    // opacity is only supported on macOS
+    backgroundColor: '#262626',
+
+    // terminal selection color
+    selectionColor: 'rgba(248,28,229,0.3)',
+
+    // border color (window, tabs)
+    borderColor: '#444',
+
+    // custom CSS to embed in the main window
+    css: ``,
+  
+
+    // custom CSS to embed in the terminal window
+    termCSS: '',
+  
+    // if you're using a Linux setup which show native menus, set to false
+    // default: `true` on Linux, `true` on Windows, ignored on macOS
+    showHamburgerMenu: true,
+
+    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
+    showWindowControls: true,
+
+    // custom padding (CSS format, i.e.: `top right bottom left`)
+    padding: `40px 40px`,
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#000000',
+      red: '#C51E14',
+      green: '#1DC121',
+      yellow: '#C7C329',
+      blue: '#0A2FC4',
+      magenta: '#C839C5',
+      cyan: '#20C5C6',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#6A76FB',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    // make sure to use a full path if the binary name doesn't work
+    // (e.g `C:\\Windows\\System32\\bash.exe` instad of just `bash.exe`)
+    // if you're using powershell, make sure to remove the `--login` below
+    //shell: 'C:\\Users\\ydai\\AppData\\Local\\Programs\\Git\\git-cmd.exe',
+    shell: 'cmd.exe',
+    shellArgs: ['/k', 'C:\\Users\\ydai\\Documents\\cmder\\vendor\\init.bat'],
+    env: { 'TERM':'cygwin' }, // required for git-for-windows
+    
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    //shellArgs: ['--command=usr/bin/bash.exe', '-l', '-i']
+
+    // for environment variables
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: true
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyperterm-monokai',
+    'hyper-tab-icons',
+    'hypercwd',
+    'hyper-dark-scrollbar',
+    'hyper-blink'
+    ],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+ 	"editor:copy": "ctrl+c",
+  	"editor:paste": "ctrl+v",
+	"editor:break": "ctrl+shift+c",
+  },
+};
